0b3d47460fab540c0858531b2c4627951dc5612e
Shashank2512318 <73745703+Shashank2512318@users.noreply.github.com>
2020-12-30 18:35:23 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,30 @@
+class Pig extends Base {
+    constructor(x, y) {
+        
+        super(x, y, 50, 50, 0);
+
+        this.Image= loadImage("sprites/enemy.png")
+
+        this.visible= (255);
+    }
+
+    display() {
+       console.log(this.body.speed);
+       
+       if(this.body.speed<2) {
+         
+        super.display();
+
+       } else{
+        push();
+        World.remove(myworld, this.body);
+        this.visible= this.visible-10
+        tint(255, this.visible);
+        image(this.Image, this.body.position.x, this.body.position.y, 50, 50)
+        pop();
+       }
+        
+    }
+
+    
+}
